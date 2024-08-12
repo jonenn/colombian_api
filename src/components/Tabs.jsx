c538cc155d15be06a7eb3bfdf65901030d83bf90
Juan J Malaver <juanmagri@outlook.es>
2024-08-12 13:39:31 -0500

    General layout for tab menu and content

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
+import '../styles/Tabs.css';
 
 function Tabs({ tabs }) {
    console.log(tabs);
    const [tab, setTab] = useState(0);
 
    return (
-      <div>
-         {tabs.map((item, index) => {
-            return (
-               <div key={index} onClick={() => setTab(index)}>
-                  {item.label}
-               </div>
-            );
-         })}
+      <>
+         <div className="menu-container">
+            {tabs.map((item, index) => {
+               return (
+                  <div key={index} onClick={() => setTab(index)}>
+                     {item.label}
+                  </div>
+               );
+            })}
+         </div>
          {console.log(tab)}
          <div>{tabs[tab] && tabs[tab].content}</div>
-      </div>
+      </>
    );
 }
 
