7bd77eab68093740af3f53a5de6aad5ef0cbd4aa
Juan J Malaver <juanmagri@outlook.es>
2024-08-12 18:43:54 -0500

    Changed the base url for the project with vite config

--- src/components/AccordionTitle.jsx
+++ src/components/AccordionTitle.jsx
@@ -11,7 +11,7 @@ function AccordionTitle({ children, onClick, style, expanded }) {
          >
             <path
                d="M7.35746 6.76427C7.83093 6.36475 7.83093 5.63525 7.35746 5.23573L2.21072 0.89281C1.56054 0.344178 0.565814 0.806353 0.565814 1.65708L0.565815 10.3429C0.565815 11.1936 1.56054 11.6558 2.21072 11.1072L7.35746 6.76427Z"
-               fill="#A7AAC2"
+               fill="#254061"
             />
          </svg>
          <div className="accordion-title" onClick={onClick}>
